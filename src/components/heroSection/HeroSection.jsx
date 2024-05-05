6804abc75eb2da54ec252d68ed379382397e2464
nuks7743 <116757191+nuks7743@users.noreply.github.com>
2024-05-05 18:24:30 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
     return (
         <div>
-           <img className=" h-44 lg:h-full w-full" src="https://i.ibb.co/DpF0msH/banner.jpg" alt="" />
+           <img className=" h-44 lg:h-full w-full" src="https://ibb.co/M6HFKWx" alt="" />
         </div>
     );
 }
